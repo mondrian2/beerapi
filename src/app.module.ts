24a04a424ec2d99012dc6eec5cee1060c3e75ee0
daniel <deecomunicacao@gmail.com>
2023-03-05 19:41:56 -0300

    static

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommandsModule } from './commands/commands.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://cestaspoa:cestaspoa@cluster0.l58w9wa.mongodb.net/cestaspoa?retryWrites=true&w=majority',
     ),
